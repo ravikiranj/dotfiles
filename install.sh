86e5f229e50f7910a8a45e7b6404993f4a6ff50f
Ravikiran Janardhana <rjanardhana@tripadvisor.com>
2016-03-30 13:54:05 -0700

    Added bashrc shortcuts

--- install.sh
+++ install.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 
 # Copy files
-echo "Copying files"
+echo "Copying files and shortcuts"
 cp ./vimrc ~/.vimrc
 cp ./tmux.conf ~/.tmux.conf
 cp ./screenrc ~/.screenrc
 cp ./ackrc ~/.ackrc
 cp ./vrapperrc ~/.vrapperrc
 cp ./ideavimrc ~/.ideavimrc
+cat ./bashrc_shortcuts >> ~/.bashrc_shortcuts
 echo "Done."
 
 # Post install notes
 echo "Install below packages using your beloved package manager"
-echo "<pkg_manager> install vim tmux screen ack/ack-grep"
+echo "<pkg_manager> install vim tmux screen colordiff ack/ack-grep"
 echo "Run \"vim\" to configure plugins"
