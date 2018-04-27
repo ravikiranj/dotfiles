ade9487742d9bb94edcac052fd17bcf7c528ae7a
Ravikiran Janardhana <ravijan@amazon.com>
2018-04-27 16:55:01 -0700

    Added Dockerfile

---
+++ Dockerfile
@@ -0,0 +1,8 @@
+FROM ubuntu:18.04
+
+RUN apt-get update && apt-get install -y sudo && rm -rf /var/lib/apt/lists/*
+
+ADD . /opt/ravikiranj/dotfiles
+WORKDIR /opt/ravikiranj/dotfiles
+
+CMD ["bash", "/opt/ravikiranj/dotfiles/install.sh"]
--- install.sh
+++ install.sh
@@ -10,7 +10,14 @@ cp ./screenrc $HOME/.screenrc
 cp ./ackrc $HOME/.ackrc
 cp ./vrapperrc $HOME/.vrapperrc
 cp ./ideavimrc $HOME/.ideavimrc
-cat ./bashrc_shortcuts >> $HOME/.bashrc
+
+if grep -v '# Custom Aliases added by ravikiranj dotfiles' ~/.bashrc > /dev/null; then
+    cat ./bashrc_shortcuts >> $HOME/.bashrc
+fi
+
+if grep -v '# Custom Aliases added by ravikiranj dotfiles' ~/.zshrc > /dev/null; then
+    cat ./zshrc_shortcuts >> $HOME/.zshrc
+fi
 echo "Done."
 
 # Post install notes
@@ -24,31 +31,64 @@ if [ -f /etc/lsb-release  ]; then
 fi
 
 function install_tpm() {
+    echo "Installing tmux plugin manager"
     git clone https://github.com/tmux-plugins/tpm $HOME/.tmux/plugins/tpm
     echo "Install tpm plugins via Ctrl+A + I"
 }
 
+function install_ohmyzsh() {
+    echo "Installing ohmyzsh"
+    sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+    echo "Installing powerlevel9k theme for zsh"
+    git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
+
+    echo "Changing theme to powerlevel9k"
+
+    if [ -f $HOME/.zshrc ]; then
+        sed -i.bak 's/^ZSH_THEME=.*/ZSH_THEME="powerlevel9k\/powerlevel9k"/' $HOME/.zshrc
+    fi
+}
+
+function install_powerline_fonts() {
+    pushd tmp
+
+    # clone
+    git clone https://github.com/powerline/fonts.git --depth=1
+
+    # install
+    cd fonts
+    ./install.sh
+
+    popd
+}
+
 if [ "$PACKAGE_MANAGER" == "yum" ]; then
     echo "Installing packages via $PACKAGE_MANAGER"
-    sudo $PACKAGE_MANAGER install vim tmux screen colordiff ack the_silver_searcher
+    sudo $PACKAGE_MANAGER install git vim tmux screen colordiff ack the_silver_searcher curl zsh
     install_tpm
+    install_ohmyzsh
+    install_powerline_fonts
 elif [ "$PACKAGE_MANAGER" == "apt-get" ]; then
     echo "Installing packages via $PACKAGE_MANAGER"
-    sudo $PACKAGE_MANAGER install vim tmux screen colordiff ack-grep silversearcher-ag
+    sudo apt-get update && sudo $PACKAGE_MANAGER install -y git vim tmux screen colordiff ack-grep silversearcher-ag fonts-powerline curl zsh
     install_tpm
+    install_ohmyzsh
 elif [ "$(uname)" == "Darwin" ]; then
     which brew
     if [ $? -eq "0" ]; then
         echo "brew is installed"
-        brew install vim tmux screen colordiff ack the_silver_searcher
+        brew install git vim tmux screen colordiff ack the_silver_searcher curl zsh
         install_tpm
+        install_ohmyzsh
+        install_powerline_fonts
     else
         echo "brew is not installed, please install brew by following instructions at https://brew.sh/ and run install.sh again"
     fi
 else
-    echo "Install below packages using your beloved package manager"
-    echo "<PACKAGE_MANAGER> install vim tmux screen colordiff ack/ack-grep"
-    echo "Run \"vim\" to configure plugins"
+    echo "Your operating system is not supported for installing packages, sorry!"
+    exit 1
 fi
 
-echo "Your system setup is complete."
+echo "Your system setup is complete. Press any key to exit"
+read value
