e3a09080601615415e6406369f98fefc612f9136
Ravikiran Janardhana <ravijan@amazon.com>
2018-04-27 17:14:43 -0700

    Fixed bugs in installing powerline fonts and ohmyzsh

--- install.sh
+++ install.sh
@@ -11,13 +11,10 @@ cp ./ackrc $HOME/.ackrc
 cp ./vrapperrc $HOME/.vrapperrc
 cp ./ideavimrc $HOME/.ideavimrc
 
-if grep -v '# Custom Aliases added by ravikiranj dotfiles' ~/.bashrc > /dev/null; then
+if grep -v '# Custom Aliases added by ravikiranj dotfiles' $HOME/.bashrc > /dev/null; then
     cat ./bashrc_shortcuts >> $HOME/.bashrc
 fi
 
-if grep -v '# Custom Aliases added by ravikiranj dotfiles' ~/.zshrc > /dev/null; then
-    cat ./zshrc_shortcuts >> $HOME/.zshrc
-fi
 echo "Done."
 
 # Post install notes
@@ -36,22 +33,30 @@ function install_tpm() {
     echo "Install tpm plugins via Ctrl+A + I"
 }
 
+function add_shortcuts_to_zshrc() {
+    if grep -v '# Custom Aliases added by ravikiranj dotfiles' $HOME/.zshrc > /dev/null; then
+        cat ./zshrc_shortcuts >> $HOME/.zshrc
+    fi
+}
+
 function install_ohmyzsh() {
     echo "Installing ohmyzsh"
     sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
     echo "Installing powerlevel9k theme for zsh"
-    git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
+    git clone https://github.com/bhilburn/powerlevel9k.git $HOME/.oh-my-zsh/custom/themes/powerlevel9k
 
     echo "Changing theme to powerlevel9k"
 
     if [ -f $HOME/.zshrc ]; then
         sed -i.bak 's/^ZSH_THEME=.*/ZSH_THEME="powerlevel9k\/powerlevel9k"/' $HOME/.zshrc
     fi
+
+    add_shortcuts_to_zshrc
 }
 
 function install_powerline_fonts() {
-    pushd tmp
+    pushd /tmp
 
     # clone
     git clone https://github.com/powerline/fonts.git --depth=1
@@ -67,20 +72,21 @@ if [ "$PACKAGE_MANAGER" == "yum" ]; then
     echo "Installing packages via $PACKAGE_MANAGER"
     sudo $PACKAGE_MANAGER install git vim tmux screen colordiff ack the_silver_searcher curl zsh
     install_tpm
-    install_ohmyzsh
+    install_ohmyzsh || echo "ohmyzsh setup might have failed, please check!"
     install_powerline_fonts
 elif [ "$PACKAGE_MANAGER" == "apt-get" ]; then
     echo "Installing packages via $PACKAGE_MANAGER"
-    sudo apt-get update && sudo $PACKAGE_MANAGER install -y git vim tmux screen colordiff ack-grep silversearcher-ag fonts-powerline curl zsh
+    sudo apt-get update && sudo $PACKAGE_MANAGER install -y git vim tmux screen colordiff ack-grep silversearcher-ag curl zsh
     install_tpm
-    install_ohmyzsh
+    install_ohmyzsh || echo "ohmyzsh setup might have failed, please check!"
+    install_powerline_fonts
 elif [ "$(uname)" == "Darwin" ]; then
     which brew
     if [ $? -eq "0" ]; then
         echo "brew is installed"
         brew install git vim tmux screen colordiff ack the_silver_searcher curl zsh
         install_tpm
-        install_ohmyzsh
+        install_ohmyzsh || echo "ohmyzsh setup might have failed, please check!"
         install_powerline_fonts
     else
         echo "brew is not installed, please install brew by following instructions at https://brew.sh/ and run install.sh again"
