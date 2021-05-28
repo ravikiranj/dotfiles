f08641ecbd482329a6f333eebff460d812f0a9d0
Ravikiran Janardhana <ravijan@amazon.com>
2021-05-28 14:00:24 -0700

    Added install_nerd_fonts.sh

--- install.sh
+++ install.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
-set -e
+#set -e
 
 # Copy files
 echo "Copying files and shortcuts"
-cp ./vimrc $HOME/.vimrc
-cp ./tmux.conf $HOME/.tmux.conf
-cp ./screenrc $HOME/.screenrc
-cp ./ackrc $HOME/.ackrc
-cp ./vrapperrc $HOME/.vrapperrc
-cp ./ideavimrc $HOME/.ideavimrc
+cp ./vimrc "$HOME/.vimrc"
+cp ./tmux.conf "$HOME/.tmux.conf"
+cp ./screenrc "$HOME/.screenrc"
+cp ./ackrc "$HOME/.ackrc"
+cp ./vrapperrc "$HOME/.vrapperrc"
+cp ./ideavimrc "$HOME/.ideavimrc"
 
-if grep -v '# Custom Aliases added by ravikiranj dotfiles' $HOME/.bashrc > /dev/null; then
-    cat ./bashrc_shortcuts >> $HOME/.bashrc
+if grep -v '# Custom Aliases added by ravikiranj dotfiles' "$HOME/.bashrc" > /dev/null; then
+    cat ./bashrc_shortcuts >> "$HOME/.bashrc"
 fi
 
 echo "Done."
@@ -29,29 +29,41 @@ fi
 
 function install_tpm() {
     echo "Installing tmux plugin manager"
-    git clone https://github.com/tmux-plugins/tpm $HOME/.tmux/plugins/tpm
+    git clone https://github.com/tmux-plugins/tpm "$HOME/.tmux/plugins/tpm"
     echo "Install tpm plugins via Ctrl+A + I"
 }
 
 function add_shortcuts_to_zshrc() {
-    if grep -v '# Custom Aliases added by ravikiranj dotfiles' $HOME/.zshrc > /dev/null; then
-        cat ./zshrc_shortcuts >> $HOME/.zshrc
+    echo "Adding shortcuts to zshrc"
+    ZSHRC_CONFIG="$HOME/.zshrc"
+    if [ ! -f "${ZSHRC_CONFIG}" ]; then
+        touch ${ZSHRC_CONFIG}
+        
     fi
+    if grep -v '# Custom Aliases added by ravikiranj dotfiles' "${ZSHRC_CONFIG}" > /dev/null; then
+        cat ./zshrc_shortcuts >> "${ZSHRC_CONFIG}"
+    fi
+}
+
+function install_powerlevel10k() {
+    echo "Installing powerlevel10k theme for zsh"
+    git clone --depth=1 https://github.com/romkatv/powerlevel10k.git "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k"
+
+    echo "Changing theme to powerlevel10k"
+
+    if [ -f "$HOME/.zshrc" ]; then
+        sed -i.bak 's/^ZSH_THEME=.*/ZSH_THEME="powerlevel10k\/powerlevel10k"/' "$HOME/.zshrc"
+    fi
+
+    echo "Copying p10k configuration"
+    cp ./p10k.zsh "$HOME/.p10k.zsh"
 }
 
 function install_ohmyzsh() {
     echo "Installing ohmyzsh"
-    sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh) --unattended"
-
-    echo "Installing powerlevel9k theme for zsh"
-    git clone https://github.com/bhilburn/powerlevel9k.git $HOME/.oh-my-zsh/custom/themes/powerlevel9k
-
-    echo "Changing theme to powerlevel9k"
-
-    if [ -f $HOME/.zshrc ]; then
-        sed -i.bak 's/^ZSH_THEME=.*/ZSH_THEME="powerlevel9k\/powerlevel9k"/' $HOME/.zshrc
-    fi
-
+    #bash -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+    git clone https://github.com/ohmyzsh/ohmyzsh.git "$HOME/.oh-my-zsh"
+    
     add_shortcuts_to_zshrc
 }
 
@@ -68,26 +80,33 @@ function install_powerline_fonts() {
     popd
 }
 
+function install_nerd_fonts() {
+    ./install_nerd_fonts.sh
+}
+
 if [ "$PACKAGE_MANAGER" == "yum" ]; then
     echo "Installing packages via $PACKAGE_MANAGER"
     sudo $PACKAGE_MANAGER install git vim tmux screen colordiff ack the_silver_searcher curl zsh ripgrep
     install_tpm
     install_ohmyzsh || echo "ohmyzsh setup might have failed, please check!"
     install_powerline_fonts
+    install_nerd_fonts
 elif [ "$PACKAGE_MANAGER" == "apt-get" ]; then
     echo "Installing packages via $PACKAGE_MANAGER"
     sudo apt-get update && sudo $PACKAGE_MANAGER install -y git vim tmux screen colordiff ack-grep silversearcher-ag curl zsh ripgrep
+    sudo apt-get update
     install_tpm
     install_ohmyzsh || echo "ohmyzsh setup might have failed, please check!"
-    install_powerline_fonts
+    #install_powerline_fonts
+    install_nerd_fonts
 elif [ "$(uname)" == "Darwin" ]; then
-    which brew
-    if [ $? -eq "0" ]; then
+    if which brew; then
         echo "brew is installed"
         brew install git vim tmux screen colordiff ack the_silver_searcher curl zsh ripgrep
         install_tpm
         install_ohmyzsh || echo "ohmyzsh setup might have failed, please check!"
         install_powerline_fonts
+        install_nerd_fonts
     else
         echo "brew is not installed, please install brew by following instructions at https://brew.sh/ and run install.sh again"
     fi
@@ -97,4 +116,4 @@ else
 fi
 
 echo "Your system dotfiles setup is complete."
-echo "You should now change your default shell to zsh (chsh) and install tpm plugins (Ctrl+A + I)."
+echo "You should now change your default shell to zsh (chsh), install tpm plugins (Ctrl+A + I) and change terminal font to Meslo Nerd Font."
